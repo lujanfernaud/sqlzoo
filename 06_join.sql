a73031e4282364e569a829b2617df51969a58d5f
lujanfernaud <lujanfernaud@gmail.com>
2017-08-11 08:23:44 +0100

    Modify comments in lesson 6

--- 06_join.sql
+++ 06_join.sql
@@ -1,9 +1,5 @@
--- 1. The first example shows the goal scored by a player with the last name
--- 'Bender'. The * says to list all the columns in the table - a shorter way
--- of saying matchid, teamid, player, gtime
-
--- Modify it to show the matchid and player name for all goals scored by
--- Germany. To identify German players, check for: teamid = 'GER'
+-- 1. Modify the code to show the matchid and player name for all goals scored
+-- by Germany. To identify German players, check for: teamid = 'GER'
 
 SELECT matchid, player
   FROM goal
@@ -15,11 +11,8 @@ SELECT id, stadium, team1, team2
   FROM game
  WHERE id = 1012;
 
--- 3. The code below shows the player (from the goal) and stadium name
--- (from the game table) for every goal scored.
-
--- Modify it to show the player, teamid, stadium and mdate and for every
--- German goal.
+-- 3. Modify the code to show the player, teamid, stadium and mdate and for
+-- every German goal.
 
 SELECT player, teamid, stadium, mdate
   FROM game JOIN goal ON id = matchid
